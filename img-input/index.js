3076021d11c1028fbba51f6ab14daa1ff04d1fad
Abdullah Rasheed <bhatti253r@gmail.com>
2024-08-30 17:55:24 +0500

    style: upload an image on frontend same time at backend

--- img-input/index.js
+++ img-input/index.js
@@ -1,5 +1,10 @@
 let profileImg = document.getElementById("profile-img");
 let inputImg = document.getElementById("input-img");
+let labelImg = document.getElementById("img-label");
 inputImg.onchange = () => {
   profileImg.src = URL.createObjectURL(inputImg.files[0]);
+  if(profileImg.src){
+    labelImg.style.display = "none";
+}
 };
+
